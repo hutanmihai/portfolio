18cb47e8e8b7c62858b9b17e08bb15895f1f7ca2
hutanmihai <hutanmihai29@gmail.com>
2023-12-24 17:59:22 +0200

    Hotfix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-const { keys } = require('lodash')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
   modularizeImports: {
     '@mui/material': {
       transform: '@mui/material/{{member}}',
+    },
+    '@mui/lab': {
+      transform: '@mui/lab/{{member}}',
     },
   },
   webpack(config) {
